e572edf811a7dd93bf846132aee3a8f2b1da5969
Jason Boon <jason.boon@student.rocva.nl>
2022-01-20 14:22:22 +0100

    second food test (works!)

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -4,15 +4,30 @@
 
 def fav_foods(thing):
     import time
+    #got this from https://www.guru99.com/python-time-sleep-delay.html
 
     if (thing.lower() == "sushi"):
         print("Wow, you must really know me well.")
         time.sleep(1.5)
-        print("Sushi is my number 1 favorite type of food.")
-        time.sleep(1)
+        print("Sushi is my most favorite type of food!")
+        time.sleep(1.5)
         print("It's just so freaking delicious...")
         time.sleep(2)
         print("Maybe you can guess number 2 and 3 on my list? :)")
+        time.sleep(2.5)
+        fav_foods(input("Can you guess my favorite foods? "))
+    
+    #if (thing.lower() == "fish sticks" or "fishsticks"): didn't work the I wanted so I did a dirty fix
+    if (thing.lower() == "fish sticks"):
+        print("Wow, you must reallyCHANGE.")
+        time.sleep(1.5)
+        print("Sushi is my most favorite type of food!")
+        time.sleep(1.5)
+        print("It's just so freaking delicious...")
+        time.sleep(2)
+        print("Maybe you can guess number 2 and 3 on my list? :)")
+        time.sleep(2.5)
+        fav_foods(input("Can you guess my favorite foods? "))
     
 
 
