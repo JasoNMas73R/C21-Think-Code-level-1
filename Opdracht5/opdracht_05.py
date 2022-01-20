54df1e938aca742ad8e74ae41c227c7b5de37f98
Jason Boon <jason.boon@student.rocva.nl>
2022-01-20 14:10:03 +0100

    first food added

--- Opdracht5/opdracht_05.py
+++ Opdracht5/opdracht_05.py
@@ -3,8 +3,16 @@
 #weet je niet meer hoe? Check het solution filmpje op: https://www.linkedin.com/learning/programming-foundations-fundamentals-3/solution-favorite-cities
 
 def fav_foods(thing):
+    import time
+
     if (thing.lower() == "sushi"):
-        print("Yes, it's works. Even with a capital letter.")
+        print("Wow, you must really know me well.")
+        time.sleep(1.5)
+        print("Sushi is my number 1 favorite type of food.")
+        time.sleep(1)
+        print("It's just so freaking delicious...")
+        time.sleep(2)
+        print("Maybe you can guess number 2 and 3 on my list? :)")
     
 
 
